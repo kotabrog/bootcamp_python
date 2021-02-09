a13f0c5f0b2f08b375ce5e3e062e697dce365567
kota <suzuki.kota0331@gmail.com>
2021-02-09 23:47:14 +0900

    add ex01

---
+++ module02/ex01/main.py
@@ -0,0 +1,44 @@
+def what_are_the_vars(*args, **kwargs):
+    try:
+        obj = ObjectC()
+        attr_count = 0
+        for value in args:
+            setattr(obj, 'var_' + str(attr_count), value)
+            attr_count += 1
+        for key, value in kwargs.items():
+            if hasattr(obj, key):
+                raise Exception()
+            setattr(obj, key, value)
+        return obj
+    except Exception:
+        return None
+
+
+class ObjectC(object):
+    def __init__(self):
+        pass
+
+
+def doom_printer(obj):
+    if obj is None:
+        print("ERROR")
+        print("end")
+        return
+    for attr in dir(obj):
+        if attr[0] != '_':
+            value = getattr(obj, attr)
+            print("{}: {}".format(attr, value))
+    print("end")
+
+
+if __name__ == "__main__":
+    obj = what_are_the_vars(7)
+    doom_printer(obj)
+    obj = what_are_the_vars("ft_lol", "Hi")
+    doom_printer(obj)
+    obj = what_are_the_vars()
+    doom_printer(obj)
+    obj = what_are_the_vars(12, "Yes", [0, 0, 0], a=10, hello="world")
+    doom_printer(obj)
+    obj = what_are_the_vars(42, a=10, var_0="world")
+    doom_printer(obj)
